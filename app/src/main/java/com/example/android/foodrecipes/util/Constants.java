87b2e9e48a91b4f58ee28fae6c25f1ad200eeea1
atharva121 <atharvasaraf@gmail.com>
2019-07-12 18:26:06 +0530

    3rd commit

---
+++ app/src/main/java/com/example/android/foodrecipes/util/Constants.java
@@ -0,0 +1,6 @@
+package com.example.android.foodrecipes.util;
+
+public class Constants {
+    public static final String BASE_URL = "htts://www.food2fork.com";
+    public static final String API_KEY = "3a03f406d9e4f509fa2a471a1cbada6e";
+}

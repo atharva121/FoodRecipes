c1ba4a56d68a1733fb22711261ff18847ddc9b92
atharva121 <atharvasaraf@gmail.com>
2019-07-17 00:43:51 +0530

    9th commit

--- app/src/main/java/com/example/android/foodrecipes/RecipeListActivity.java
+++ app/src/main/java/com/example/android/foodrecipes/RecipeListActivity.java
@@ -2,9 +2,27 @@ package com.example.android.foodrecipes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import com.example.android.foodrecipes.models.Recipe;
+import com.example.android.foodrecipes.requests.RecipeApi;
+import com.example.android.foodrecipes.requests.ServiceGenerator;
+import com.example.android.foodrecipes.requests.responses.RecipeResponse;
+import com.example.android.foodrecipes.requests.responses.RecipeSearchResponse;
+import com.example.android.foodrecipes.util.Constants;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class RecipeListActivity extends BaseActivity {
+
+    private static final String TAG = "RecipeListActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,12 +31,70 @@ public class RecipeListActivity extends BaseActivity {
         findViewById(R.id.test).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (mProgressBar.getVisibility() == View.VISIBLE){
-                    showProgressBar(false);
+                testRetrofitRequest();
+            }
+        });
+    }
+
+    private void testRetrofitRequest(){
+        RecipeApi recipeApi = ServiceGenerator.getRecipeApi();
+        /*Call<RecipeSearchResponse> responseCall = recipeApi
+                .searchRecipe(
+                        Constants.API_KEY,
+                        "chicken breast",
+                        "1"
+                );
+        responseCall.enqueue(new Callback<RecipeSearchResponse>() {
+            @Override
+            public void onResponse(Call<RecipeSearchResponse> call, Response<RecipeSearchResponse> response) {
+                Log.d(TAG, "onResponse: server response: " + response.toString());
+                if (response.code() == 200){
+                    Log.d(TAG, "onResponse: " + response.body().toString());
+                    List<Recipe> recipes = new ArrayList<>(response.body().getRecipes());
+                    for (Recipe recipe : recipes){
+                        Log.d(TAG, "onResponse: " + recipe.getTitle());
+                    }
                 }
                 else {
-                    showProgressBar(true);
+                    try {
+                        Log.d(TAG, "onResponse: " + response.errorBody().string());
+                    }catch (IOException e){
+                        e.printStackTrace();
+                    }
                 }
+            }
+
+            @Override
+            public void onFailure(Call<RecipeSearchResponse> call, Throwable t) {
+
+            }
+        });*/
+        Call<RecipeResponse> responseCall = recipeApi
+                .getRecipe(
+                        Constants.API_KEY,
+                        "8c0314"
+                );
+        responseCall.enqueue(new Callback<RecipeResponse>() {
+            @Override
+            public void onResponse(Call<RecipeResponse> call, Response<RecipeResponse> response) {
+                Log.d(TAG, "onResponse: server response: " + response.toString());
+                if (response.code() == 200){
+                    Log.d(TAG, "onResponse: " + response.body().toString());
+                    Recipe recipe = response.body().getRecipe();
+                    Log.d(TAG, "onResponse: Retrieved recipe: " + recipe.toString());
+                }
+                else {
+                    try {
+                        Log.d(TAG, "onResponse: " + response.errorBody().string());
+                    }catch (IOException e){
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<RecipeResponse> call, Throwable t) {
+
             }
         });
     }

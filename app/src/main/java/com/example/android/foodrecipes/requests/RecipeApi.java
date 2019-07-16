0daa9bb592a4a210d7c31ede25fc93558d726e08
atharva121 <atharvasaraf@gmail.com>
2019-07-16 23:31:03 +0530

    8th commit

--- app/src/main/java/com/example/android/foodrecipes/requests/RecipeApi.java
+++ app/src/main/java/com/example/android/foodrecipes/requests/RecipeApi.java
@@ -1,4 +1,25 @@
 package com.example.android.foodrecipes.requests;
 
+import com.example.android.foodrecipes.requests.responses.RecipeResponse;
+import com.example.android.foodrecipes.requests.responses.RecipeSearchResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface RecipeApi {
+    // Search
+    @GET("api/search")
+    Call<RecipeSearchResponse> searchRecipe(
+            @Query("key") String key,
+            @Query("q") String query,
+            @Query("page") String page
+    );
+
+    // Get
+    @GET("api/get")
+    Call<RecipeResponse> getRecipe(
+            @Query("key") String key,
+            @Query("rId") String recipe_id
+    );
 }

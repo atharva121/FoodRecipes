79be381bc55a8510f7b53584bcad8e6f6f25e97e
atharva121 <atharvasaraf@gmail.com>
2019-07-24 16:40:27 +0530

    14th commit

--- app/src/main/java/com/example/android/foodrecipes/requests/RecipeApiClient.java
+++ app/src/main/java/com/example/android/foodrecipes/requests/RecipeApiClient.java
@@ -3,9 +3,14 @@ package com.example.android.foodrecipes.requests;
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
 
+import com.example.android.foodrecipes.AppExecutors;
 import com.example.android.foodrecipes.models.Recipe;
 
 import java.util.List;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+import static com.example.android.foodrecipes.util.Constants.NETWORK_TIMEOUT;
 
 public class RecipeApiClient {
     private static RecipeApiClient instance;
@@ -24,5 +29,22 @@ public class RecipeApiClient {
 
     public LiveData<List<Recipe>> getRecipes(){
         return mRecipes;
+    }
+
+    public void searchRecipesApi(){
+        final Future handler = AppExecutors.getInstance().networkIO().submit(new Runnable() {
+            @Override
+            public void run() {
+                // retrieve data from rest api
+//                mRecipes.postValue();
+            }
+        });
+        AppExecutors.getInstance().networkIO().schedule(new Runnable() {
+            @Override
+            public void run() {
+                // let the user know its timed out
+                handler.cancel(true);
+            }
+        }, NETWORK_TIMEOUT, TimeUnit.MILLISECONDS);
     }
 }
--- app/src/main/java/com/example/android/foodrecipes/util/Constants.java
+++ app/src/main/java/com/example/android/foodrecipes/util/Constants.java
@@ -2,5 +2,6 @@ package com.example.android.foodrecipes.util;
 
 public class Constants {
     public static final String BASE_URL = "https://www.food2fork.com";
+    public static final int NETWORK_TIMEOUT = 3000;
     public static final String API_KEY = "3a03f406d9e4f509fa2a471a1cbada6e";
 }

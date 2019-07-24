ff86fc7fad743c030b470228bfff96205fd86a03
atharva121 <atharvasaraf@gmail.com>
2019-07-25 00:48:40 +0530

    15th commit

--- app/src/main/java/com/example/android/foodrecipes/requests/RecipeApiClient.java
+++ app/src/main/java/com/example/android/foodrecipes/requests/RecipeApiClient.java
@@ -2,19 +2,29 @@ package com.example.android.foodrecipes.requests;
 
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
+import android.util.Log;
 
 import com.example.android.foodrecipes.AppExecutors;
 import com.example.android.foodrecipes.models.Recipe;
+import com.example.android.foodrecipes.requests.responses.RecipeSearchResponse;
+import com.example.android.foodrecipes.util.Constants;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
+
+import retrofit2.Call;
+import retrofit2.Response;
 
 import static com.example.android.foodrecipes.util.Constants.NETWORK_TIMEOUT;
 
 public class RecipeApiClient {
     private static RecipeApiClient instance;
     private MutableLiveData<List<Recipe>> mRecipes;
+    private static final String TAG = "RecipeApiClient";
+    private RetrieveRecipesRunnable mRetrieveRecipesRunnable;
 
     public static RecipeApiClient getInstance(){
         if (instance == null){
@@ -31,14 +41,12 @@ public class RecipeApiClient {
         return mRecipes;
     }
 
-    public void searchRecipesApi(){
-        final Future handler = AppExecutors.getInstance().networkIO().submit(new Runnable() {
-            @Override
-            public void run() {
-                // retrieve data from rest api
-//                mRecipes.postValue();
-            }
-        });
+    public void searchRecipesApi(String query, int pageNumber){
+        if (mRetrieveRecipesRunnable != null){
+            mRetrieveRecipesRunnable = null;
+        }
+        mRetrieveRecipesRunnable = new RetrieveRecipesRunnable(query, pageNumber);
+        final Future handler = AppExecutors.getInstance().networkIO().submit(mRetrieveRecipesRunnable);
         AppExecutors.getInstance().networkIO().schedule(new Runnable() {
             @Override
             public void run() {
@@ -46,5 +54,60 @@ public class RecipeApiClient {
                 handler.cancel(true);
             }
         }, NETWORK_TIMEOUT, TimeUnit.MILLISECONDS);
+    }
+
+    private class RetrieveRecipesRunnable implements Runnable{
+
+        private String query;
+        private int pageNumber;
+        boolean cancelRequest;
+
+        public RetrieveRecipesRunnable(String query, int pageNumber) {
+            this.query = query;
+            this.pageNumber = pageNumber;
+            cancelRequest = false;
+        }
+
+        @Override
+        public void run() {
+            try {
+                Response response = getRecipes(query, pageNumber).execute();
+                if (cancelRequest){
+                    return;
+                }
+                if (response.code() == 200){
+                    List<Recipe> list = new ArrayList<>(((RecipeSearchResponse)response.body()).getRecipes());
+                    if (pageNumber == 1){
+                        mRecipes.postValue(list);
+                    }
+                    else {
+                        List<Recipe> currentRecipes = mRecipes.getValue();
+                        currentRecipes.addAll(list);
+                        mRecipes.postValue(currentRecipes);
+                    }
+                }
+                else {
+                    String error = response.errorBody().string();
+                    Log.e(TAG, "run: " + error);
+                    mRecipes.postValue(null);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                mRecipes.postValue(null);
+            }
+        }
+
+        private Call<RecipeSearchResponse> getRecipes(String query, int pageNumber){
+            return ServiceGenerator.getRecipeApi().searchRecipe(
+                    Constants.API_KEY,
+                    query,
+                    String.valueOf(pageNumber)
+            );
+        }
+
+        private void cancelRequest(){
+            Log.d(TAG, "cancelRequest: canceling the search request.");
+            cancelRequest = true;
+        }
     }
 }
